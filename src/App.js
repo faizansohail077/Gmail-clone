5a857a191f9e72f41fd0e0caed3b21cce5f21b94
Muhammad Faizan <faizansohail077@gmail.com>
2021-02-21 20:01:13 +0500

    completed sidebar working on emailList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
-
+import Sidebar from './components/Sidebar/Sidebar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Mail from './Screens/Mail/Mail';
+import EmailList from './Screens/EmailList/EmailList';
 function App() {
   return (
-    <div className="App">
-      <Header />
-      Gmail Clone
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <div className="app__body">
+          <div>
+            <Sidebar />
+          </div>
+          <Switch>
+            <div>
+              <Route path="/mail"  >
+                <Mail />
+              </Route>
+              <Route path="/" exact >
+                <EmailList />
+              </Route>
+            </div>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Screens/Mail/Mail.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Mail.css'
+const Mail = () => {
+    return (
+        <div className="mail">
+            MailMailMail
+        </div>
+    )
+}
+
+export default Mail
